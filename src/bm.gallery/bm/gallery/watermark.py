0a80dc4a6b431a307c33c03b5723b40cfc050999
Rob Miller <rob@mochimedia.com>
2011-02-07 22:33:18 -0800

    delete code instead of commenting it out.

--- src/bm.gallery/bm/gallery/watermark.py
+++ src/bm.gallery/bm/gallery/watermark.py
@@ -10,9 +10,6 @@ class Watermark(ImageProcessor):
         if image.mode != 'RGBA':
             image = image.convert('RGBA')
 
-        # pink = (255, 94, 200)
-        # white = (255, 255, 255)
-
         im_width, im_height = image.size
         copyright = "Copyright \xa9 by the Artist and Burning Man"
 
@@ -22,21 +19,6 @@ class Watermark(ImageProcessor):
                        fill=(0,0,0,90))
         draw.text((10, im_height - 15), copyright, fill=(255,255,255,90))
         newimage = Image.new('RGB', (im_width, im_height))
-
-        # newimage = Image.new('RGB', (im_width, im_height + 50), white)
-        # draw2 = ImageDraw.Draw(newimage)
-        # draw2.text((10, im_height + 5),
-        #            "\xa9 All images are copyright in their respective year, by "
-        #            "both the ",
-        #            fill=pink)
-        # draw2.text((10, im_height + 15),
-        #            "photographer and Burning Man. For publication or other use ",
-        #            fill=pink)
-        # draw2.text((10, im_height + 25),
-        #            "requests, contact the photographer at the email provided and ",
-        #            fill=pink)
-        # draw2.text((10, im_height + 35),
-        #            "press@burningman.com for written permission.", fill=pink)
 
         comp = Image.composite(overlay, image, overlay)
         newimage.paste(comp, (0,0))
