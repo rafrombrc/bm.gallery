aefd3852241c5f1ab55ceb290386efb5471c8439
Rob Miller <rob@kalistra.com>
2011-06-27 23:26:29 -0700

    add ldap_delete function

--- src/bm.gallery/bm/gallery/ldap_util.py
+++ src/bm.gallery/bm/gallery/ldap_util.py
@@ -2,11 +2,13 @@ import ldap
 from django.conf import settings
 from bm.gallery.utils import encode_modlist_strings
 
+
 def get_ldap_connection():
     ldapper = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
     ldapper.simple_bind_s(who=settings.AUTH_LDAP_BIND_DN,
                            cred=settings.AUTH_LDAP_BIND_PASSWORD)
     return ldapper
+
 
 def get_user_dn(username):
     return 'uid=%s,ou=galleries,dc=burningman,dc=com' % username
@@ -17,4 +19,10 @@ def ldap_add(dn, modlist_map):
     encode_modlist_strings(modlist_map)
     modlist = ldap.modlist.addModlist(modlist_map)
     ldapper.add_s(dn, modlist)
+    ldapper.unbind_s()
+
+
+def ldap_delete(dn):
+    ldapper = get_ldap_connection()
+    ldapper.delete_s(dn)
     ldapper.unbind_s()
