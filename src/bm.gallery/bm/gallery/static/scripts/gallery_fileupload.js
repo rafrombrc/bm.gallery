5ca0008fd94a458d40fc300d9d7e773a14192c3f
Bruce <bruce@ecomsmith.com>
2012-02-14 15:03:32 -0800

    Making file uploader use sequential mode, so as not to overwhelm the server.

--- src/bm.gallery/bm/gallery/static/scripts/gallery_fileupload.js
+++ src/bm.gallery/bm/gallery/static/scripts/gallery_fileupload.js
@@ -32,7 +32,8 @@ var gallery = {
         }
         gallery.fileelement.fileupload({
             dropZone: $(element),
-            acceptFileTypes: types
+            acceptFileTypes: types,
+            sequentialUploads: true
         }).bind('fileuploaddone',function(e, data) {
             if (data.textStatus === 'success') {
                 gallery.lastdata = data;
@@ -41,27 +42,6 @@ var gallery = {
             }
             return true;
         });
-
-        // // Load existing files:
-        // $.getJSON($(element + ' form').prop('action'), function (files) {
-        //     var fu = $('#fileupload').data('fileupload');
-        //     fu._adjustMaxNumberOfFiles(-files.length);
-        //     fu._renderDownload(files)
-        //         .appendTo($(element + ' .files'))
-        //         .fadeIn(function () {
-        //             // Fix for IE7 and lower:
-        //             $(this).show();
-        //         });
-        // });
-
-        // // Open download dialogs via iframes,
-        // // to prevent aborting current uploads:
-        // $(element + ' .files a:not([target^=_blank])').live('click', function (e) {
-        //     e.preventDefault();
-        //     $('<iframe style="display:none;"></iframe>')
-        //         .prop('src', this.href)
-        //         .appendTo('body');
-        // });
     },
 
     show_continue : function(data) {
